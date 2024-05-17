bc2b881730c0e6bf2ce13d464cf38b8fa7e06d7b
Kyle Kelley <rgbkrk@gmail.com>
2024-05-17 15:27:24 -0700

    start creating a runtimes crate that will track all the kernels

--- crates/language/src/language_settings.rs
+++ crates/language/src/language_settings.rs
@@ -53,6 +53,7 @@ pub fn all_language_settings<'a>(
 pub struct AllLanguageSettings {
     /// The inline completion settings.
     pub inline_completions: InlineCompletionSettings,
+    pub enable_runtimes: bool,
     defaults: LanguageSettings,
     languages: HashMap<Arc<str>, LanguageSettings>,
     pub(crate) file_types: HashMap<Arc<str>, Vec<String>>,
@@ -114,6 +115,8 @@ pub struct LanguageSettings {
     pub always_treat_brackets_as_autoclosed: bool,
     /// Which code actions to run on save
     pub code_actions_on_format: HashMap<String, bool>,
+    /// Whether to enable interactive runtimes for this language.
+    pub enable_runtimes: bool,
 }
 
 impl LanguageSettings {
@@ -619,6 +622,7 @@ impl settings::Settings for AllLanguageSettings {
         }
 
         Ok(Self {
+            enable_runtimes: true,
             inline_completions: InlineCompletionSettings {
                 provider: if let Some(provider) = inline_completion_provider {
                     provider
